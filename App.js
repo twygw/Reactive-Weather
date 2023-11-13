7d1227235db4bc5a08b9cd7863cd4ad0744f7dc8
twygw <stanley.dix@sky.uk>
2023-11-13 16:29:27 +0000

    reactive weather part 1

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 // Import data and WeatherCard here
-
+import cities from "./data";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
-
-
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {cities.map((city) => (
+          <City
+            name={city.city}
+            temp={city.temperature}
+            forcast={city.forecast}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
